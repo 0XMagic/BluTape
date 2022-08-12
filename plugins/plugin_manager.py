9cfcc64fe6d62ddfc0b8c5982cbde4fdf8f33f65
JacobKessler <Javadotdet@gmail.com>
2022-08-12 10:39:41 -0400

    made the Plugins tab accessible from plugin_manager.plugin_tab

--- plugins/plugin_manager.py
+++ plugins/plugin_manager.py
@@ -10,28 +10,13 @@ plugins = {
 		for x in (info.path / "plugins").iterdir() if
 		(x.suffix == ".py" or x.suffix == ".disabled") and x.stem not in ("__init__", "plugin_manager")
 }
+plugin_tab = tk.Menu(
+		application.top_bar, tearoff = 0, background = COLOR_BACKGROUND_ALT, foreground = COLOR_TEXT_GENERIC)
 
 
 def init():
-	top_bar_plugin_manager = tk.Menu(
-			application.top_bar,
-			tearoff = 0,
-			background = COLOR_BACKGROUND_ALT,
-			foreground = COLOR_TEXT_GENERIC
-	)
-
-	top_bar_plugin_manager.add_command(
-			label = "Manager Plugins",
-			command = open_manager_window,
-			font = application.top_bar_font
-	)
-
-	application.top_bar.add_cascade(
-
-			label = "Plugins",
-			menu = top_bar_plugin_manager,
-			font = application.top_bar_font
-	)
+	plugin_tab.add_command(label = "Manager Plugins", command = open_manager_window, font = application.top_bar_font)
+	application.top_bar.add_cascade(label = "Plugins", menu = plugin_tab, font = application.top_bar_font)
 	pass
 
 
@@ -44,15 +29,11 @@ class ManagerWindowInstance:
 		self.window = tk.Toplevel(master, bg = COLOR_BACKGROUND)
 		self.window.wm_title("Plugin Manager")
 		self.window.protocol("WM_DELETE_WINDOW", self.on_close)
-
 		self.window.grab_set()
 		self.window.focus_set()
 		self.window.geometry("640x480")
-
 		self.stuff = list()
-
 		self.window.grid_columnconfigure(0, weight = 1)
-
 		lbl = tk.Label(
 				self.window, text = "Plugin changes are effective on restart", fg = COLOR_TEXT_GENERIC,
 				bg = COLOR_BACKGROUND)
@@ -62,12 +43,9 @@ class ManagerWindowInstance:
 			f = tk.Frame(self.window, bg = COLOR_BACKGROUND)
 			b = tk.Button(
 					f, text = k + ": " + ("ENABLED" if v["is_enabled"] else "DISABLED"),
-					command = lambda: self.on_press(k, n),
-					bg = COLOR_TEXT_STR if v["is_enabled"] else COLOR_ERROR
-			)
+					command = lambda: self.on_press(k, n), bg = COLOR_TEXT_STR if v["is_enabled"] else COLOR_ERROR)
 			b.grid(column = 0, row = 0)
 			f.grid(column = 0, row = n + 1)
-
 			self.stuff.append(b)
 
 	def on_press(self, k, btn):
